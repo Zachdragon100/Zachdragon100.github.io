ce0d15422dc1d96f3c6411cf7b70b7cb190e9e26
Zachdragon100 <Zachdragon10@hotmail.com>
2014-03-07 11:32:49 -0800

    Removal of Sleep and Crash
    TEST

--- notatetrisclone.js
+++ notatetrisclone.js
@@ -1,23 +1,7 @@
 var isrunning = true;
 while (isrunning){
 		
-	
-	
-	
-	
-	
-	
-	
-	sleep(5000);
-	
 	isrunning = false;
 	
 }
 
-function sleep(millis, callback) {
-    setTimeout(function()
-            { callback(); }
-    , millis);
-}
-
-document.write("Oops! The Game Crashed!");
