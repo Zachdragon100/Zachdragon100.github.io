569592247e8c1c81bc4bfe252a81776c9ae3ac9b
Zachdragon100 <Zachdragon10@hotmail.com>
2014-04-25 09:25:15 -0700

    Second Circle

--- JavaScript.js
+++ JavaScript.js
@@ -1,10 +1,29 @@
-
-var canvasThing= new function(){
-	var canvas =	document.getElementsByClassName("canvasOne");
-	var ctx =		canvas.getContext('2d');
+function canvasThing(){
+	// Find the canvas element.
+	var canvas = document.getElementById('tutorial');
 	
-	var width = canvas.getAttribute("width");
-	var height = canvas.getAttribute("height");
+	// Determine size and find center of canvas.
+	var w = canvas.getAttribute("width");
+	var h = canvas.getAttribute("height");
+	var x = w / 2;
+	var y = h / 2;
+	var gap = 0.95;
+	
+	// Determine how large the circle can be.
+	var r = (x > y ? y : x) - 10;
+	
+	if (canvas.getContext){
+		var ctx = canvas.getContext('2d');
+		
+		ctx.beginPath();
+		
+		// Outer circle.
+		ctx.arc(x, y, r, 0, Math.PI*2, true);
+		ctx.moveto(x + r * gap, y);
+		ctx.arc(x, y, r * gap, 0, Math.PI * 2, true);
+		ctx.stroke();
+            }
+        }
 	
 };
 
