13fa1402ceb7787c3025a346d962d57113071d8a
Zachdragon100 <Zachdragon10@hotmail.com>
2014-04-30 17:36:11 -0700

    Minor Edits

--- JavaScript.js
+++ JavaScript.js
@@ -11,8 +11,8 @@ function draw(){
 		var wedgesCount = 6; //prompt("Number of Parts");
 		var allAround = 2 * Math.PI;
 		var rad = (centerX < centerY ? centerX : centerY) - 10;
-		var colourSet = ["#8C4400", "#00A7E6", "#E67D02", "#E6D836", "#E64E99", "#0EBF23", "#FFFFFF", "#000000"];
-		var playerNumber = 1;
+		var colourSet = ["#8C4400", "#00A7E6", "#E67D02", "#E6D836", "#E64E99", "#0EBF23", "#FFFFFF", "#222222"];
+		var playerNumber = 2;
 		try{
 		ctx.beginPath();
 		ctx.fillStyle = colourSet[playerNumber - 1];
@@ -31,53 +31,53 @@ catch(err)
   }
 	}
 	
-	function drawWedges(ctx, x, y, r, n, player){
-		var n = Math.abs(n);
-		var n = Math.ceil(n);
-		var n = n%257;
-		var angle = 2 * Math.PI / n;
-		var allAround = 2 * Math.PI;
-		var xOffset = 0;
-		var yOffset = 0;
-		var colourSet = ["#A55D00", "#15C0FF", "#FF961B", "#FFF14F", "#FF67B2", "#27D83C", "#FFFFFF", "#000000"];
-		var colourSet2 = ["#722A00", "#008DCC", "#CC6300", "#CCBE1C", "#CC347F", "#00A509", "#FFFFFF", "#000000"];
-		var getsColours = [true, false, true, false, true, true];
-		
-		
-		
-		
-		if(n!=1){
-			for(i=0; i<n; i++){
-				var conPointX = x + r * Math.cos(i * angle);
-				var conPointY = y + r * Math.sin(i * angle);
-				
-				var innerXoff = r *Math.cos((i + .5) * angle) / 10;
-				var innerYoff = r *Math.sin((i + .5) * angle) / 10;
-				
-				
-				
-				ctx.beginPath();
-				ctx.moveTo(x + xOffset + innerXoff, y + yOffset + innerYoff);
-				ctx.lineTo(conPointX, conPointY);
-				ctx.arc(x, y, r, i * angle, (i+1) * angle, false);
-				ctx.lineTo(x + xOffset + innerXoff, y + yOffset + innerYoff);
-				ctx.closePath();
-				if(getsColours[i]){
-					ctx.fillStyle = colourSet[i];
-				}else{
-					ctx.fillStyle = colourSet2[player - 1];
-				}
-				ctx.fill();
-				ctx.stroke();
-			}
+function drawWedges(ctx, x, y, r, n, player){
+	var n = Math.abs(n);
+	var n = Math.ceil(n);
+	var n = n%257;
+	var angle = 2 * Math.PI / n;
+	var allAround = 2 * Math.PI;
+	var xOffset = 0;
+	var yOffset = 0;
+	var colourSet = ["#A55D00", "#15C0FF", "#FF961B", "#FFF14F", "#FF67B2", "#27D83C", "#FFFFFF", "#000000"];
+	var colourSet2 = ["#722A00", "#008DCC", "#CC6300", "#CCBE1C", "#CC347F", "#00A509", "#CCCCCC", "#000000"];
+	var getsColours = [true, false, true, false, true, true];
+	
+	
+	
+	
+	if(n!=1){
+		for(i=0; i<n; i++){
+			var conPointX = x + r * Math.cos(i * angle);
+			var conPointY = y + r * Math.sin(i * angle);
 			
-		}else{
+			var innerXoff = r *Math.cos((i + .5) * angle) / 10;
+			var innerYoff = r *Math.sin((i + .5) * angle) / 10;
+			
+			
+			
 			ctx.beginPath();
-			ctx.moveTo(x+r, y)
-			ctx.arc(x, y, r, 0, allAround, true);
-			ctx.fillStyle = colourSet[0];
+			ctx.moveTo(x + xOffset + innerXoff, y + yOffset + innerYoff);
+			ctx.lineTo(conPointX, conPointY);
+			ctx.arc(x, y, r, i * angle, (i+1) * angle, false);
+			ctx.lineTo(x + xOffset + innerXoff, y + yOffset + innerYoff);
+			ctx.closePath();
+			if(getsColours[i]){
+				ctx.fillStyle = colourSet[i];
+			}else{
+				ctx.fillStyle = colourSet2[player - 1];
+			}
 			ctx.fill();
 			ctx.stroke();
 		}
 		
+	}else{
+		ctx.beginPath();
+		ctx.moveTo(x+r, y)
+		ctx.arc(x, y, r, 0, allAround, true);
+		ctx.fillStyle = colourSet[0];
+		ctx.fill();
+		ctx.stroke();
 	}
+	
+}
