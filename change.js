40e9d1ada690937cc6a67824749656b6db40712a
Zachdragon100 <Zachdragon10@hotmail.com>
2014-05-17 23:56:27 -0700

    Turn And Face The Strain
    Added pictures of dice!

--- change.js
+++ change.js
@@ -2,9 +2,13 @@
 
 function change() {
 
-	var divCh = document.getElementById("divToChange");
+	var imgCh = document.getElementById("imgToChange");
 
 	var number = Math.ceil(6 * Math.random());
-	divCh.innerHTML = number; //Will convert into Picture ASAP
+	
+	var changeTo = "images/die_faces/png/" + number + ".png";
+	console.log(changeTo);
+	
+	imgCh.src = changeTo; //Will convert into Picture ASAP
 
 }
