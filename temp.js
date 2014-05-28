5a9c5ee27ac9965618f582035c50003a0bd62651
Zachdragon100 <Zachdragon10@hotmail.com>
2014-05-28 13:32:31 -0700

    Rename temporarily

--- temp.js
+++ temp.js
@@ -5,10 +5,10 @@ function change() {
 	var imgCh = document.getElementById("imgToChange");
 
 	var number = Math.ceil(6 * Math.random());
-	
+
 	var changeTo = "images/die_faces/png/" + number + ".png";
 	console.log(changeTo);
-	
+
 	imgCh.src = changeTo; //Will convert into Picture ASAP
 
 }
