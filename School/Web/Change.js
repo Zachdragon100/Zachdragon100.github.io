25483bd3879f20df44cfd478c26bb595262200e0
Zachdragon100 <Zachdragon10@hotmail.com>
2014-06-08 13:44:07 -0700

    Rearrange A Lot

--- School/Web/Change.js
+++ School/Web/Change.js
@@ -6,7 +6,7 @@ function change() {
 
 	var number = Math.ceil(6 * Math.random());
 
-	var changeTo = "images/die_faces/png/" + number + ".png";
+	var changeTo = "../../images/die_faces/png/" + number + ".png";
 	console.log(changeTo);
 
 	imgCh.src = changeTo; //Will convert into Picture ASAP
